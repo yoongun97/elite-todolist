61830ee310d504b47dd21574e86e2d7ee2129b79
Cho Yoongeon <tesung5470@naver.com>
2023-07-07 16:57:02 +0900

    좋아요 버튼 ui 추가, style-components 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,101 @@
-// import logo from "./logo.svg";
-import "./App.css";
 import { useState } from "react";
+import { styled } from "styled-components";
+import uuid from "react-uuid";
+
+const ListLayout = styled.div`
+  padding: 20px;
+`;
+
+const TodoInput = styled.input`
+  border: 1px solid #e5e5e5;
+  border-radius: 5px;
+  font-size: 15px;
+  height: 30px;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: 1px solid #e5e5e5;
+  border-radius: 5px;
+  width: 50px;
+  height: 30px;
+
+  cursor: pointer;
+`;
+
+const ListContainer = styled.div`
+  border: 1px solid #e5e5e5;
+  margin: 20px 0 20px 0;
+  width: 70%;
+`;
+
+const TodoCard = styled.div`
+  margin: 10px;
+
+  display: flex;
+  justify-content: space-between;
+`;
 
 function App() {
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState(["리액트 공부하기", "아침 운동하기"]);
+  const [todos, setTodos] = useState([
+    { id: uuid(), title: "리액트 공부하기", isLike: false },
+    { id: uuid(), title: "아침 운동하기", isLike: false },
+  ]);
 
   const addTodosHandler = () => {
-    const newTodo = [...todos, input];
-    setTodos(newTodo);
+    const newTodo = { title: input, isLike: false };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
   };
 
-  const deleteTOdoHandler = (inIndex) => {
-    const updateTodos = todos.filter((todo, index) => {
-      return index !== inIndex;
+  const deleteTOdoHandler = (id) => {
+    const updateTodos = todos.filter((todo) => {
+      return todo.id !== id;
     });
     setTodos(updateTodos);
   };
 
   return (
     <>
-      <input
-        value={input}
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          addTodosHandler();
-          setInput("");
-        }}
-      >
-        출력
-      </button>
-      <h4>투두리스트</h4>
-      {todos.map((todo, index) => {
-        return (
-          <div>
-            <span>{todo}</span>
-            <button
-              onClick={() => {
-                deleteTOdoHandler(index);
-              }}
-            >
-              삭제
-            </button>
-          </div>
-        );
-      })}
+      <ListLayout>
+        <h2>투두리스트</h2>
+        <TodoInput
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        />
+        <Button
+          onClick={() => {
+            addTodosHandler();
+            setInput("");
+          }}
+        >
+          추가
+        </Button>
+        <ListContainer>
+          {todos.map((todo) => {
+            return (
+              <TodoCard key={todo.id}>
+                <span>{todo.title}</span>
+                <div>
+                  <Button
+                    onClick={() => {
+                      deleteTOdoHandler(todo.id);
+                    }}
+                  >
+                    삭제
+                  </Button>
+                  <button>좋아요</button>
+                  <span>1</span>
+                </div>
+              </TodoCard>
+            );
+          })}
+        </ListContainer>
+      </ListLayout>
     </>
   );
 }
